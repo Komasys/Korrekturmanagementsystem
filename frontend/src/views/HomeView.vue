1562ec8e0a5e8f3e339a977b93c039da1cd4f514
Dominik <dominik.k276@gmail.com>
2025-03-15 18:21:53 +0100

    feat: update HomeView with welcome message and CI/CD test button

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p>Test</p>
+    <p>Welcome to the Korrekturmanagementsystem!</p>
+    <button @click="testCICD">Test CI/CD</button>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'HomeView',
+  methods: {
+    testCICD() {
+      alert('CI/CD is working!')
+    },
+  },
+}
+</script>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #369870;
+}
+</style>
